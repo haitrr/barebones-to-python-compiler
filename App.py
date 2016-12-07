2ab9917d3ad499601a036ec380bfb612fa47d9a1
Hai Tran <vroyibg@gmail.com>
2016-12-07 20:42:29 +0700

    Rename project

--- App.py
+++ App.py
@@ -1,33 +1,33 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 import Parser
-import Interpreter
+import Compiler
 from subprocess import call
 from tkinter import filedialog
 import os
 
 
-def interpreter():
-    interpretered_code_text_box.config(state=NORMAL)
+def compiler():
+    compilered_code_text_box.config(state=NORMAL)
     try:
         ast = Parser.parse(source_code_text_box.get(1.0,END), verbose=False)
     except Exception as e:
-        interpretered_code_text_box.delete(1.0, END)
-        interpretered_code_text_box.insert(INSERT,e )
+        compilered_code_text_box.delete(1.0, END)
+        compilered_code_text_box.insert(INSERT,e )
         run_button.config(state=DISABLED)
     else:
-        result = Interpreter.interpreter(ast)
-        Interpreter.get_variable(ast)
-        input_code = Interpreter.require_input()
-        output_code = Interpreter.output_variables()
-        subtract_function = Interpreter.define_subtract()
+        result = Compiler.compiler(ast)
+        Compiler.get_variable(ast)
+        input_code = Compiler.require_input()
+        output_code = Compiler.output_variables()
+        subtract_function = Compiler.define_subtract()
         result =subtract_function+ input_code + result + output_code
-        interpretered_code_text_box.delete(1.0,END)
-        interpretered_code_text_box.insert(INSERT,result)
+        compilered_code_text_box.delete(1.0,END)
+        compilered_code_text_box.insert(INSERT,result)
         save_result(result)
         run_button.config(state=NORMAL)
     finally:
-        interpretered_code_text_box.config(state=DISABLED)
+        compilered_code_text_box.config(state=DISABLED)
 
 def save_result(text):
     file = open("temp.py",'w')
@@ -41,7 +41,7 @@ def get_input():
     source_code_text_box.insert(INSERT,file.read())
     file.close()
 def run():
-    os.system("python temp.py")
+    call("python temp.py",shell = True)
 root = Tk()
 root.resizable(width=False, height=False)
 root.grid()
@@ -50,16 +50,16 @@ open_file_button.grid(row=0, column=0)
 run_button = Button(root,text = "Run",command=run)
 run_button.config(state=DISABLED)
 run_button.grid(row=3,column=0,columnspan=2)
-interpreter_button = Button(root,text = "Interpreter",command = interpreter)
-interpreter_button.grid(row=0, column=1)
+compiler_button = Button(root, text = "Compile", command = compiler)
+compiler_button.grid(row=0, column=1)
 source_code_lable=Label(root,text="Source code")
 source_code_lable.grid(row=1,column=0)
-interpretered_code_lable = Label(root,text="Result")
-interpretered_code_lable.grid(row =1 ,column=1)
+compilered_code_lable = Label(root,text="Result")
+compilered_code_lable.grid(row =1 ,column=1)
 source_code_text_box = ScrolledText(root,width = 50,height=30)
 source_code_text_box.grid(row =2,column = 0)
-interpretered_code_text_box = ScrolledText(root,width = 50,height=30,state=DISABLED)
-interpretered_code_text_box.grid(row =2 ,column = 1)
+compilered_code_text_box = ScrolledText(root,width = 50,height=30,state=DISABLED)
+compilered_code_text_box.grid(row =2 ,column = 1)
 #root.geometry('{}x{}'.format(500, 500))
-root.title("BareBones Interpreter")
+root.title("BareBones Compiler")
 root.mainloop()
